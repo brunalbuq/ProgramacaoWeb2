8d0aa04cd03cff652f595bada9bb0de0aa543c77
Bruna de albuquerque medeiros <bruna.albuquerque@dinfor.uniceub>
2023-05-15 10:08:20 -0300

    Introdução ao Node

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log("acesso o sistema back"); //https://replit.com/
+const express = require("express");
+const app = express ();
+const path = require("path");
+var __dirname = path.resolve();
+const mysql = require("mysql");
+const shal = require("shal");
+
+//app.use(express.static(__dirname+"/views"));
+app.engine("html", require('ejs').renderFile);
+
+var bodyParser = require('body-parser');
+app.use = (bodyParser.urlencoded({extedend: false }));
+
+const connection = mysql.createConnection({
+  host:"104.18.112.45",
+  user:"id20370795_brunalbuq",
+  password:"************",
+  port:"3306", //porta padrão mysql
+  database:"id20370795_dba_pw"
+});
+
+app.get("/", function(req, res) {
+  connection.query ("select * from usuario", function(erro, linhas, colunas) {
+    if(erro) {
+      console.log ("O servidor de banco de dados está fora do ar, fiz um mock para mostrar a tela do usuário.")
+      //console.log (erro.mensage)
+      const linhas = [{"id": "1", "nome": "Bruna", "endereco": "Brasília", "cpf": "074.003.251-83", "senha": "********"},
+      {"id": "2", "nome": "Yasmin", "endereco": "Brasília", "cpf": "049.806.951-67", "senha": "********"}];
+        res.render(__dirname + "/views/index.html", { usuarios: linhas })
+    }
+    else {
+      res.render(__dirname + "/views/index.html", { usuarios: linhas })
+    }
+  })
+})
